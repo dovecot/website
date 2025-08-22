466d991dc3aa3e4452c7fa3646fc986153b19189
Aki Tuomi <aki.tuomi@open-xchange.com>
2025-08-22 08:53:42 +0300

    pigeonhole: Add pigeonhole website

---
+++ pigeonhole/.vitepress/config.mts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vitepress'
+
+import UnoCSS from 'unocss/vite'
+import unoConfig from './unocss.config.mts'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Pigeonhole |  Sieve Support for Dovecot",
+  description: "Pigeonhole is the name of the project that adds support for the Sieve language (RFC 5228) and the ManageSieve protocol (RFC 5804) to the Dovecot Secure IMAP Server.",
+  lang: 'en-US',
+  base: '/',
+  cleanUrls: true,
+  metaChunk: true,
+
+  themeConfig: {
+    nav: [
+      { 'text': 'Download', link: 'download.html' },
+      { 'text': 'Documentation', link: 'https://doc.dovecot.org/' },
+      { 'text': 'Dovecot', link: 'https://dovecot.org/' },
+    ],
+  },
+
+  vite: {
+    optimizeDeps: {
+      exclude: ['vitepress'],
+    },
+    server: {
+      hmr: {
+        overlay: false,
+      },
+    },
+    plugins: [
+      UnoCSS(unoConfig),
+    ],
+  },
+})
---
+++ pigeonhole/.vitepress/theme/Layout.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import { useData, withBase } from 'vitepress'
+
+type NavItem = NavItemWithLink | NavItemWithChildren
+
+interface NavItemWithLink {
+  text: string
+  link: string
+  activeMatch?: string
+  target?: string
+  rel?: string
+  noIcon?: boolean
+}
+
+interface NavItemChildren {
+  text?: string
+  items: NavItemWithLink[]
+}
+
+interface NavItemWithChildren {
+  text?: string
+  items: (NavItemChildren | NavItemWithLink)[]
+  activeMatch?: string
+}
+
+const { site, theme, frontmatter } = useData()
+
+const nav = (theme.value.nav as NavItem[]) || []
+
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-col bg-white m-x-32">
+    <!-- Wrapper for consistent layout -->
+    <div class="mx-auto w-full px-4">
+      <!-- Navbar -->
+      <header class="w-full border-b border-gray-400">
+        <nav class="flex items-center justify-between py-3">
+          <!-- Left: Logo -->
+          <div class="flex items-center gap-2">
+ 	    <a :href="withBase('/index')"><img src="/pigeonhole_logo.png" alt="Pigeonhole" class="h-32 w-auto" /></a>
+          </div>
+
+          <!-- Right: Nav Links -->
+          <div class="flex items-center gap-6">
+            <a
+              v-for="item in nav"
+              :key="item.link"
+	      :href="item.link.startsWith('http') ? item.link : withBase(item.link)"
+              class="text-gray-700 hover:underline text-lg"
+            >
+              {{ item.text }}
+            </a>
+          </div>
+        </nav>
+      </header>
+
+      <!-- Main content -->
+      <main class="w-full mx-auto px-4 py-10 flex">
+        <article class="prose w-full mx-auto max-w-8xl flex">
+          <Content/>
+        </article>
+      </main>
+    </div>
+
+   <footer class="max-w-full w-full py-4 text-center text-sm text-gray-500 bg-white">
+	   &copy; Dovecot - See <a class="underline" href="https://github.com/dovecot/website" rel="external" target="_blank">https://github.com/dovecot/website</a>
+  </footer>
+  </div>
+</template>
